2608ca9071d315626251b68b48e1f27e7ab973fc
Jorgee-hub <jmontiek217@gmail.com>
2026-06-14 15:09:32 +0000

    finalizacion de puntos extras eliminacion de redundancia y comprobacion de cada boton, trabajo finalizado

--- src/js/components/Counter.jsx
+++ src/js/components/Counter.jsx
@@ -1,18 +1,57 @@
 function Counter(props) {
-
     const hh = String(props.hours).padStart(2, "0")
     const mm = String(props.minutes).padStart(2, "0")
     const ss = String(props.seconds).padStart(2, "0")
     return (
-        <div className="justify-content-center gap-1 bg-black d-flex align-items-center">
-            <i className="fs-6 rounded p-1 bg-dark text-light bi bi-clock"></i>
-            <div className="rounded p-1 bg-dark text-light">{hh[0]}</div>
-            <div className="rounded p-1 bg-dark text-light">{hh[1]}</div>
-            <div className="rounded p-1 bg-dark text-light">{mm[0]}</div>
-            <div className="rounded p-1 bg-dark text-light">{mm[1]}</div>
-            <div className="rounded p-1 bg-dark text-light">{ss[0]}</div>
-            <div className="rounded p-1 bg-dark text-light">{ss[1]}</div>
-        </div>
+        <>
+            <h1 className="text-light bg bg-dark d-flex justify-content-center fw-bold m-0">Contador de segundos by Jorgee-hub</h1>
+            <div className="justify-content-center gap-3 bg-black d-flex align-items-center" style={{ fontSize: "80px" }}>
+                <i className="rounded p-1 bg-dark text-light bi bi-clock" style={{ width: "70px", height: "96px", textAlign: "center", fontSize: "60px" }}></i>
+                <div className="my-2 rounded p-1 bg-dark text-light" style={{ width: "70px", height: "96px", textAlign: "center", fontSize: "60px" }}>{hh[0]}</div>
+                <div className="my-2 rounded p-1 bg-dark text-light" style={{ width: "70px", height: "96px", textAlign: "center", fontSize: "60px" }}>{hh[1]}</div>
+                <div className="my-2 rounded p-1 bg-dark text-light" style={{ width: "70px", height: "96px", textAlign: "center", fontSize: "60px" }}>{mm[0]}</div>
+                <div className="my-2 rounded p-1 bg-dark text-light" style={{ width: "70px", height: "96px", textAlign: "center", fontSize: "60px" }}>{mm[1]}</div>
+                <div className="my-2 rounded p-1 bg-dark text-light" style={{ width: "70px", height: "96px", textAlign: "center", fontSize: "60px" }}>{ss[0]}</div>
+                <div className="my-2 rounded p-1 bg-dark text-light" style={{ width: "70px", height: "96px", textAlign: "center", fontSize: "60px" }}>{ss[1]}</div>
+            </div>
+            <div className="d-flex justify-content-center">
+                <button className="btn btn-dark fs-2 m-1" onClick={props.stopInterval}>Detener</button>
+                <button className="btn btn-dark fs-2 m-1" onClick={props.startInterval}>Reanudar</button>
+                <button className="btn btn-dark fs-2 m-1" onClick={props.resetInterval}>Reiniciar</button>
+            </div>
+
+            <div className="m-1 d-flex justify-content-center">
+                <input
+                    className="text-light bg-dark form-control" style={{ width: "200px" }}
+                    type="number"
+                    placeholder="Establecer alerta"
+                    onChange={(e) => props.setAlertTime(e.target.value)}
+                    pattern="[0-9]*"
+                    onKeyDown={(e) => {
+                        if (!/[0-9]/.test(e.key) && e.key !== "Backspace") {
+                            e.preventDefault()
+                        }
+                    }}
+                />
+            </div>
+            <div className="d-flex justify-content-center">
+                <input
+                    className="text-light bg-dark form-control" style={{ width: "200px" }}
+                    type="number"
+                    onChange={(e) => props.setCountDownTime(e.target.value)}
+                    placeholder="Cuenta regresiva"
+                    pattern="[0-9]*"
+                    onKeyDown={(e) => {
+                        if (!/[0-9]/.test(e.key) && e.key !== "Backspace") {
+                            e.preventDefault()
+                        }
+                    }}
+                />
+                <button className="btn btn-dark fs-6 m-1" onClick={props.startCountDown}>Iniciar cuenta regresiva</button>
+
+            </div>
+
+        </>
     );
 }
 export default Counter;
--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,33 +1,74 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
-//Bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap"
-
-// index.css'
 import '../styles/index.css'
-
-// components
-import Home from './components/Home';
 import Counter from './components/Counter';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 let seconds = 0
 let minutes = 0
 let hours = 0
-setInterval(() => {
-  seconds++
-  if (seconds === 60) {
-    seconds = 0
-    minutes++
-  }
-  if (minutes === 60) {
-    minutes = 0
-    hours++
-  }
-  root.render(
-    <React.StrictMode>
-      <Counter seconds={seconds} minutes={minutes} hours={hours} />
-    </React.StrictMode>
-  )
-}, 1000)
+let interval = null
+let alertTime = 0
+let countDown = false
+let countDownTime = 0
+
+const setAlertTime = (time) => {
+  alertTime = parseInt(time)
+}
+
+const setCountDownTime = (time) => {
+  countDownTime = parseInt(time)
+}
+
+const stopInterval = () => {
+  clearInterval(interval)
+  interval = null
+}
+
+const startInterval = () => {
+  if (interval !== null) return;
+  stopInterval()
+  interval = setInterval(() => {
+    if (countDown) {
+      seconds--
+      if (seconds === -1) { seconds = 59; minutes-- }
+      if (minutes === -1) { minutes = 59; hours-- }
+      if (hours * 3600 + minutes * 60 + seconds === 0) {
+        alert("¡¡Tiempo!!")
+        countDown = false
+        stopInterval()
+      }
+    }
+    else {
+      seconds++
+      if (hours * 3600 + minutes * 60 + seconds === alertTime) alert("¡¡Alerta!!")
+      if (seconds === 60) { seconds = 0; minutes++ }
+      if (minutes === 60) { minutes = 0; hours++ }
+    }
+    root.render(
+      <React.StrictMode>
+        <Counter seconds={seconds} minutes={minutes} hours={hours} stopInterval={stopInterval} startInterval={startInterval} resetInterval={resetInterval} setAlertTime={setAlertTime} startCountDown={startCountDown} setCountDownTime={setCountDownTime} countDownTime={countDownTime} />
+      </React.StrictMode>
+    )
+  }, 1000)
+}
+
+startInterval()
+const resetInterval = () => {
+  stopInterval()
+  countDown = false
+  seconds = 0
+  minutes = 0
+  hours = 0
+  startInterval()
+}
+const startCountDown = () => {
+  hours = Math.floor(countDownTime / 3600)
+  minutes = Math.floor((countDownTime % 3600) / 60)
+  seconds = countDownTime % 60
+  countDown = true
+  stopInterval()
+  startInterval()
+}
